84c38de84a3915ec6270c5b7bd877086929c869e
MORTAADA <zamortadha33@gmail.com>
2026-08-18 17:41:14 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,15 @@
 const CACHE_NAME = 'white-wolf-scholar-v24';
 const ASSETS_TO_CACHE = [
-  './WhiteWolfScholar.html',
+  './index.html',
   './manifest.json'
 ];
 
-// تثبيت التطبيق وتخزين الملفات
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
-// جلب الملفات (من التخزين إذا كان متاحاً، أو من الشبكة)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))

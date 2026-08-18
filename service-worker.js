198abc1d929c9d8c019e1232c34cbfce69f07bc7
MORTAADA <zamortadha33@gmail.com>
2026-08-18 18:15:38 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,10 @@
-const CACHE_NAME = "white-wolf-v24";
-const ASSETS = [
-  "./index.html",
-  "./manifest.json"
-];
+const CACHE_NAME = "white-wolf-v25";
+const ASSETS = ["./index.html", "./manifest.json"];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
